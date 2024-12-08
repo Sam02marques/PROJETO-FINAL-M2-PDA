07f13737362061e6ada8c710ba1e779ac2006d15
tereza <brunaaasilvaaa261@gmail.com>
2024-12-07 22:06:56 -0300

    :iphone: Responsividade header, footer e da página about

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 //CÓDIGO GLOBAL//
+
+//navbar mobile
+const button = document.querySelector(".navMobile")
+const menu = document.querySelector(".navBarContainer");
+button.addEventListener("click", ()=>{
+menu.classList.toggle('active');
+})

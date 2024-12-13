11f88de818888b7505d600bac1e33562a85d8f7f
Mario Vinicius <marioviniciuxcs@gmail.com>
2024-12-13 19:12:44 -0300

    fix: correção de redirecionamento, media query, e importação

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,38 @@
-// CÓDIGO ESPECÍFICO DE CADA PÁGINA//
+// CÓDIGO ESPECÍFICO DO Contact.html
+// PARTE DO FUNCIONAMENTO DO MODAL
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("denunciaForm");
+    const modal = document.getElementById("modal");
+    const closeModal = document.getElementById("closeModal");
+
+    // Ao enviar o formulário
+    form.addEventListener("submit", (e) => {
+      e.preventDefault(); // Evita o reload da página
+      modal.style.display = "block"; // Mostra o modal
+      form.reset(); // Limpa os campos do formulário
+    });
+
+    // Fechar o modal ao clicar no "x"
+    closeModal.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+});
+
+// Seleciona o botão e a lista de navegação
+const navButton = document.querySelector("navMobile");
+const navBar = document.querySelector("navBarContainer");
+
+// Adiciona o evento de clique
+navButton.addEventListener("click", () => {
+  navBar.classList.toggle("active"); // Adiciona ou remove a classe "active"
+});
+
+// Fechar o menu se clicar em algum item (opcional)
+const navItems = document.querySelectorAll("listHeader li a");
+navItems.forEach(item => {
+  item.addEventListener("click", () => {
+    navBar.classList.remove("active"); // Fecha o menu
+  });
+});
+
